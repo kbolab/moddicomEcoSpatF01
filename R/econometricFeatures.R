1dec5752822aba4af2b853d04c346195d460a9de
Roberto Gatta <roberto.gatta.bs@gmail.com>
2016-05-26 19:14:38 +0200

    .

--- R/econometricFeatures.R
+++ R/econometricFeatures.R
@@ -128,3 +128,119 @@ F01.statsI <- function(moranGray){
   }
   return(Imean)
 }
+#' gg4554fv
+#'
+#' @description  dgfr
+#' @useDynLib moddicomEcoSpatF01
+#' @import fields
+#' @export
+F01.pipeline.01<-function(inputGTV,dStar, erosionMarginX, erosionMarginY, whatDoYouWantBack=c("statsI")) {
+  
+  # operazioni preliminari
+  objS<-services();
+  stats.I<-list()
+  final.result<-list()
+  inputGTV.eroded <-objS$applyErosion(inputGTV,erosionMarginX,erosionMarginY, margin.z=0)
+
+  for(possibiliResults in whatDoYouWantBack) final.result[[possibiliResults]]<-list();
+
+  # cicla su tutti i pazienti
+  for (k in seq(1,length(inputGTV))){
+    cat(c("\n Patient: ",k ) )
+    patID<-names(inputGTV)[[k]]
+    vcNonEroso <- inputGTV[[k]]$masked.images$voxelCube
+    vcEroso <- inputGTV.eroded[[k]]$masked.images$voxelCube
+    listaVoxelErosi<-which(!is.na(vcEroso),arr.ind = TRUE)
+    
+    # per prima cosa calcola la matrice W
+    wMatrix<-calcola.matrice.W(voxelCube,dStar, erosionMarginX, erosionMarginY, vcNonEroso, vcEroso, listaVoxelErosi)
+    if("wMatrix" %in% whatDoYouWantBack) final.result$wMatrix[[patID]]<-wMatrix
+    
+    # ora calcola il moranGrayMean
+    mGM<-calcola.moranGrayMean(wMatrix)
+    if("moranGrayMean" %in% whatDoYouWantBack) final.result$moranGrayMean[[patID]]<-mGM
+    
+    # ora calcola I
+    stats.I<- calcola.statsT(mGM)
+    if("statsI" %in% whatDoYouWantBack) final.result$statsI[[patID]]<-stats.I
+  }
+  return(final.result)
+}
+calcola.statsT<-function(moranGray){
+  Imean <- numeric()
+  Icoeff <- numeric()
+  for(z in seq(1:length(moranGray))){
+    if (length(moranGray[[z]]) == 0) {Icoeff[z] <- NA}
+    else {Icoeff[z] <- moranGray[[z]]}
+  }
+  Icoeff <- sapply(Icoeff,unlist)
+  Icoeff <- Icoeff[which(complete.cases(Icoeff))]
+  slice <- seq(1:length(Icoeff))
+  plot(slice,Icoeff)
+  Imean <- mean(Icoeff)
+  return(Imean)  
+}
+calcola.moranGrayMean<-function(Wlist) {
+  #ciclo sulle slice
+  moranGray<-list()
+  for(z in seq(1:length(Wlist))){
+    wStar <- Wlist[[z]]$wStar
+    coords <- Wlist[[z]]$coords
+    vcEroso <- Wlist[[z]]$vcEroso
+    x <- coords[,1]
+    y <- coords[,2]
+    grayLevels <- numeric()
+    #il minimo mi serve per portare a zero il valore minimo se negativo
+    minumGray <- min(vcEroso,na.rm=T)
+    for(i in seq(1:length(x))){
+      if (minumGray < 0 ) {grayLevels[i] <- vcEroso[x[i],y[i]] - minumGray}
+      else {grayLevels[i] <- vcEroso[x[i],y[i]]}
+    }
+    #hist(grayLevels)
+    grayMean <- mean(grayLevels)
+    # definisco la grandezza da "laggare" come differenza rispetto al valor medio
+    grayVar <- grayLevels - grayMean
+    if (length(wStar) == 0) next
+    #calcolo lo scalare di correlazione spaziale I
+    laggedGray <- wStar %*% grayVar
+    I <- grayVar %*% laggedGray
+    norm <- grayVar %*% grayVar
+    Inorm <- as.numeric(I/norm)
+    moranGray[[z]] <- list("I_coeff"= Inorm)
+  }  
+  return(moranGray)
+}
+
+calcola.matrice.W<-function(voxelCube,dStar, erosionMarginX, erosionMarginY, vcNonEroso, vcEroso, listaVoxelErosi) {
+  Wlist<-list()
+  #ora seleziono le coord (x,y) per ogni slice z e calcolo la matrice delle distanze euclidee tra le coppie di punti (funzione: rdist) e la matrice W definita nel ciclo for(i)for(j)
+  for(z in seq(1,max(listaVoxelErosi[,3]))){
+    cat(c("."))
+    slice <- listaVoxelErosi[which(listaVoxelErosi[,3]==z),]
+    if (class(slice) == "integer" || length(slice) == 0) next
+    slice2D <- slice[,1:2]
+    D <- matrix()
+    D <- rdist(slice2D)
+    W <- matrix(0,nrow=nrow(D), ncol=ncol(D))
+    Wstar <- matrix(0,nrow=nrow(W), ncol=ncol(W))
+    # invoca la funzione in C forzando esplicitamente il tipo
+    # dei parametri (importante)
+    lista.risultato<-.C("internalLoop",
+                        as.matrix(D),
+                        as.integer(nrow(D)),
+                        as.double(dStar),
+                        as.matrix(W));
+    #normalizzo i coefficienti di W
+    for (i in seq(1,nrow(lista.risultato[[4]]))){
+      Wnorm <- 0
+      for (j in seq(1,ncol(lista.risultato[[4]]))){
+        Wnorm <- Wnorm + lista.risultato[[4]][i,j]
+      }
+      Wstar[i,] <-lista.risultato[[4]][i,]/Wnorm
+    }
+    #costruisco le k*z(k) (nPazienti*nslicePaziente) matrici W
+    Wstar <- round(Wstar,2)
+    Wlist[[z]] <- list("wStar"= Wstar,"coords"= slice, "vcEroso"=vcEroso[,,z])
+  }  
+  return(Wlist)
+}
