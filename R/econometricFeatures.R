b1f30a0cf3fb94c92029f41c1fe26884fd8a9dbd
Roberto Gatta <roberto.gatta.bs@gmail.com>
2016-06-23 17:06:13 +0200

    .

--- R/econometricFeatures.R
+++ R/econometricFeatures.R
@@ -18,124 +18,9 @@
 #' wMatrix<-F01.wMatrix( inputGTV = GTV, 2,2,2)
 #'
 #' }
-F01.wMatrix <- function(inputGTV,dStar, erosionMarginX, erosionMarginY){
-  objS<-services();
-  Wlist <- list()
-  inputGTV.eroded <-objS$applyErosion(inputGTV,erosionMarginX,erosionMarginY, margin.z=0)
-  #create "eroded" mmButoStructureVoxelList object
-  #loop on patients
-
-  for (k in seq(1,length(inputGTV))){
-    cat(c("\n Patient: ",k ) )
-    patID<-names(inputGTV)[[k]]
-    Wlist[[ patID ]] <-list()
-    vcNonEroso <- inputGTV[[k]]$masked.images$voxelCube
-    vcEroso <- inputGTV.eroded[[k]]$masked.images$voxelCube
-    listaVoxelErosi<-which(!is.na(vcEroso),arr.ind = TRUE)
-
-    #ora seleziono le coord (x,y) per ogni slice z e calcolo la matrice delle distanze euclidee tra le coppie di punti (funzione: rdist) e la matrice W definita nel ciclo for(i)for(j)
-    for(z in seq(1,max(listaVoxelErosi[,3]))){
-      cat(c("."))
-      slice <- listaVoxelErosi[which(listaVoxelErosi[,3]==z),]
-      if (class(slice) == "integer" || length(slice) == 0) next
-      slice2D <- slice[,1:2]
-      D <- matrix()
-      D <- rdist(slice2D)
-      W <- matrix(0,nrow=nrow(D), ncol=ncol(D))
-      Wstar <- matrix(0,nrow=nrow(W), ncol=ncol(W))
-      # invoca la funzione in C forzando esplicitamente il tipo
-      # dei parametri (importante)
-      lista.risultato<-.C("internalLoop",
-                          as.matrix(D),
-                          as.integer(nrow(D)),
-                          as.double(dStar),
-                          as.matrix(W));
-      #normalizzo i coefficienti di W
-      for (i in seq(1,nrow(lista.risultato[[4]]))){
-        Wnorm <- 0
-        for (j in seq(1,ncol(lista.risultato[[4]]))){
-          Wnorm <- Wnorm + lista.risultato[[4]][i,j]
-        }
-        Wstar[i,] <-lista.risultato[[4]][i,]/Wnorm
-      }
-      #costruisco le k*z(k) (nPazienti*nslicePaziente) matrici W
-      Wstar <- round(Wstar,2)
-      Wlist[[ patID ]][[z]] <- list("wStar"= Wstar,"coords"= slice, "vcEroso"=vcEroso[,,z])
-    }
-    #chiuso il ciclo per le slice del paziente kesimo
-  }
-  #chiuso il ciclo sui pazienti
-  return(Wlist)
-}
-
-#' cdscdg45g54scd
 #'
-#' @description  dfdsg54g54g54g3fdfdsfdsfds
-#' @export
-F01.moranGrayMean <- function(Wlist){
-  moranGray <- list()
-  #ciclo sui pazienti
-  for(k in seq(1:length(Wlist))){
-    patID<-names(Wlist)[k]
-    moranGray[[ patID ]] <- list()
-    #ciclo sulle slice
-    for(z in seq(1:length(Wlist[[k]]))){
-      wStar <- Wlist[[k]][[z]]$wStar
-      coords <- Wlist[[k]][[z]]$coords
-      vcEroso <- Wlist[[k]][[z]]$vcEroso
-      x <- coords[,1]
-      y <- coords[,2]
-      grayLevels <- numeric()
-      #il minimo mi serve per portare a zero il valore minimo se negativo
-      minumGray <- min(vcEroso,na.rm=T)
-      for(i in seq(1:length(x))){
-        if (minumGray < 0 ) {grayLevels[i] <- vcEroso[x[i],y[i]] - minumGray}
-        else {grayLevels[i] <- vcEroso[x[i],y[i]]}
-      }
-      #hist(grayLevels)
-      grayMean <- mean(grayLevels)
-      # definisco la grandezza da "laggare" come differenza rispetto al valor medio
-      grayVar <- grayLevels - grayMean
-      if (length(wStar) == 0) next
-      #calcolo lo scalare di correlazione spaziale I
-      laggedGray <- wStar %*% grayVar
-      I <- grayVar %*% laggedGray
-      norm <- grayVar %*% grayVar
-      Inorm <- as.numeric(I/norm)
-      moranGray[[ patID ]][[z]] <- list("I_coeff"= Inorm)
-    }
-  }
-  return(moranGray)
-}
-
-#' cdscdgg5445g54scd
-#'
-#' @description  dfdsg5445g5g54g54g3fdfdsfdsfds
-#' @export
-F01.statsI <- function(moranGray){
-  Imean <- numeric()
-  for(k in seq(1:length(moranGray))){
-    Icoeff <- numeric()
-    for(z in seq(1:length(moranGray[[k]]))){
-      if (length(moranGray[[k]][[z]]) == 0) {Icoeff[z] <- NA}
-      else {Icoeff[z] <- moranGray[[k]][[z]]}
-    }
-    Icoeff <- sapply(Icoeff,unlist)
-    Icoeff <- Icoeff[which(complete.cases(Icoeff))]
-    slice <- seq(1:length(Icoeff))
-    plot(slice,Icoeff)
-    Imean[k] <- mean(Icoeff)
-  }
-  return(Imean)
-}
-#' gg4554fv
-#'
-#' @description  dgfr
-#' @useDynLib moddicomEcoSpatF01
-#' @import fields
-#' @export
 F01.pipeline.01<-function(inputGTV,dStar, erosionMarginX, erosionMarginY, whatDoYouWantBack=c("statsI")) {
-  
+
   # operazioni preliminari
   objS<-services();
   stats.I<-list()
@@ -151,40 +36,63 @@ F01.pipeline.01<-function(inputGTV,dStar, erosionMarginX, erosionMarginY, whatDo
     vcNonEroso <- inputGTV[[k]]$masked.images$voxelCube
     vcEroso <- inputGTV.eroded[[k]]$masked.images$voxelCube
     listaVoxelErosi<-which(!is.na(vcEroso),arr.ind = TRUE)
-    
+
     # per prima cosa calcola la matrice W
-    wMatrix<-calcola.matrice.W(voxelCube,dStar, erosionMarginX, erosionMarginY, vcNonEroso, vcEroso, listaVoxelErosi)
-    if("wMatrix" %in% whatDoYouWantBack) final.result$wMatrix[[patID]]<-wMatrix
-    
-    # ora calcola il moranGrayMean
-    mGM<-calcola.moranGrayMean(wMatrix)
-    if("moranGrayMean" %in% whatDoYouWantBack) final.result$moranGrayMean[[patID]]<-mGM
-    
-    # ora calcola I
-    stats.I<- calcola.statsT(mGM)
-    if("statsI" %in% whatDoYouWantBack) final.result$statsI[[patID]]<-stats.I
+    if (length(listaVoxelErosi) != 0){
+      wMatrix<-calcola.matrice.W(voxelCube,dStar, erosionMarginX, erosionMarginY, vcNonEroso, vcEroso, listaVoxelErosi)
+      if("wMatrix" %in% whatDoYouWantBack) final.result$wMatrix[[patID]]<-wMatrix
+
+     # ora calcola il moranGrayMean
+      mGM<-calcola.moranGrayMean(wMatrix)
+      if("moranGrayMean" %in% whatDoYouWantBack) final.result$moranGrayMean[[patID]]<-mGM
+
+     # ora calcola I
+      stats.I<- calcola.statsT(mGM)
+      if("statsI" %in% whatDoYouWantBack) final.result$statsI[[patID]]<-stats.I
+    }
   }
   return(final.result)
 }
+
 calcola.statsT<-function(moranGray){
   Imean <- numeric()
   Icoeff <- numeric()
+  Inull <- numeric()
+  varNull <- numeric()
+  zScores <- numeric()
+  pValues <- numeric()
   for(z in seq(1:length(moranGray))){
     if (length(moranGray[[z]]) == 0) {Icoeff[z] <- NA}
-    else {Icoeff[z] <- moranGray[[z]]}
+    else {Icoeff[z] <- moranGray[[z]]$I_coeff
+    Inull[z] <- moranGray[[z]]$I_nullHp
+    varNull[z] <- moranGray[[z]]$varI_nullHp}
   }
   Icoeff <- sapply(Icoeff,unlist)
   Icoeff <- Icoeff[which(complete.cases(Icoeff))]
+  Inull <- sapply(Inull,unlist)
+  Inull <- Inull[which(complete.cases(Inull))]
+  varNull <- sapply(varNull,unlist)
+  varNull <- varNull[which(complete.cases(varNull))]
   slice <- seq(1:length(Icoeff))
   plot(slice,Icoeff)
-  Imean <- mean(Icoeff)
-  return(Imean)  
+  for(z in seq(1:length(Icoeff))){
+    if (length(Icoeff) == length(Inull)){
+      zScores[z] <- (Icoeff[z] - Inull[z])/sqrt(varNull[z])
+      pValues[z] <- 2*pnorm(-abs(zScores[z]))
+    }
+    else skip
+  }
+  testResults <- list("zScores"= zScores, "pValues"= pValues)
+  #Imean <- mean(Icoeff)
+  return(testResults)
 }
+
+
 calcola.moranGrayMean<-function(Wlist) {
   #ciclo sulle slice
   moranGray<-list()
   for(z in seq(1:length(Wlist))){
-    wStar <- Wlist[[z]]$wStar
+    Wstar <- Wlist[[z]]$wStar
     coords <- Wlist[[z]]$coords
     vcEroso <- Wlist[[z]]$vcEroso
     x <- coords[,1]
@@ -200,14 +108,41 @@ calcola.moranGrayMean<-function(Wlist) {
     grayMean <- mean(grayLevels)
     # definisco la grandezza da "laggare" come differenza rispetto al valor medio
     grayVar <- grayLevels - grayMean
-    if (length(wStar) == 0) next
+    if (length(Wstar) == 0) next
     #calcolo lo scalare di correlazione spaziale I
-    laggedGray <- wStar %*% grayVar
+    laggedGray <- Wstar %*% grayVar
     I <- grayVar %*% laggedGray
     norm <- grayVar %*% grayVar
     Inorm <- as.numeric(I/norm)
-    moranGray[[z]] <- list("I_coeff"= Inorm)
-  }  
+
+    #### CALCOLO ANCHE IL VALOR MEDIO E LA VARIANZA DI I SOTTO L'IPOTESI NULLA
+    if (is.nan(I) == T) next
+    t_Wstar <- t(Wstar) # 'a trassposta
+    N <- nrow(Wstar)
+    expectI = 1 / ( - N + 1 );
+    S1 <- 0
+    S2 <- 0
+    S4 <- 0
+    S5 <- 0
+    sumSquaresW <- 0
+    varI <- 0
+    lista.varMoranNull<-.C("varMoranNull",
+                           as.double(Wstar),
+                           as.double(t_Wstar),
+                           as.integer(N),
+                           as.double(S1),
+                           as.double(S2));
+
+    S1 <- lista.varMoranNull[[4]]
+    S2 <- lista.varMoranNull[[5]]
+    S3 <-  nrow(Wstar) * sum((grayVar^4)) * (1/sum(grayVar^2)^2)
+    sumSquaresW <- sum(Wstar)^2
+    S4 <- (N^2 - 3 * N + 3) * S1 - N * S2 + 3 * sumSquaresW
+    S5 <- (N^2 - N) * S1 - 2 * N * S2 + 6 * sumSquaresW
+    varI <- ((N * S4 - S3 * S5 ) / ((N - 1) * (N - 2) * (N - 3) * sumSquaresW)) - expectI^2
+
+    moranGray[[z]] <- list("I_coeff"= Inorm, "I_nullHp"= expectI, "varI_nullHp"= varI)
+  }
   return(moranGray)
 }
 
@@ -240,7 +175,9 @@ calcola.matrice.W<-function(voxelCube,dStar, erosionMarginX, erosionMarginY, vcN
     }
     #costruisco le k*z(k) (nPazienti*nslicePaziente) matrici W
     Wstar <- round(Wstar,2)
+
     Wlist[[z]] <- list("wStar"= Wstar,"coords"= slice, "vcEroso"=vcEroso[,,z])
-  }  
+  }
   return(Wlist)
 }
+
--- src/moddicomEcoSpatF01.c
+++ src/moddicomEcoSpatF01.c
@@ -7,9 +7,66 @@ void internalLoop(double *D, int *dimD, double *dStar, double *W){
   N = *dimD;
   int i;
   double sum;
-  
-  for (i=0 ; i<N*N; i++){ 	
+
+  for (i=0 ; i<N*N; i++){
     if (D[i] >= *dStar || D[i] < 10e-7) { W[i] = 0.00;}
     else if (D[i] < *dStar && D[i] > 10e-7){ W[i] = pow(D[i],-2);}
   }
+}
+
+
+
+void varMoranNull(double *Wstar, double *t_Wstar, int *dimWstar, double *S1, double *S2){
+
+  int N;
+  N = *dimWstar;
+ //per S1 prendo la trasposta e sommo stesso indice
+  int i;
+  for (i=0 ; i<N*N; i++){
+    *S1 = (*S1 + pow(Wstar[i] + t_Wstar[i],2));
+     //printf("\n i=%d, Wij=%lf, Wji=%lf, S1=%lf ", i , Wstar[i], t_Wstar[i], *S1);
+  }
+  *S1 = *S1/2;
+  //printf("\n  S1=%lf",*S1);
+
+
+  // per S2 mi muovo sui primi N elementi di matrice e sua traposta --> poi il quadrato della somma --> poi la somma dei quadrati
+  int j;
+  double tmp;
+  for (i=0 ; i<N; i++){
+    for (j=0 ; j<N; j++){
+    tmp = tmp + Wstar[i * N + j] + t_Wstar[i * N + j];
+    //printf("\n i=%d, j=%d, W=%lf, S2=%lf, i * N + j=%d", i ,j, Wstar[i * N + j],*S2,i * N + j);
+    }
+    *S2 = *S2 + pow(tmp,2);
+    tmp=0;
+  }
+  //printf("\n  S2=%lf",*S2);
+
+ //Per S3 mi servono i valorio degli scarti dalla media (grayVar)
+
+//  double tmp_gray4;
+//  double tmp_gray2;
+//  for (i=0 ; i<N*N; i++){
+//    tmp_gray4 = (tmp_gray4 + pow(grayVar[i],4));
+//    tmp_gray2 = (tmp_gray2 + pow(grayVar[i],2));
+//    printf("\n tmp_gray4=%lf, tmp_gray2=%lf", tmp_gray4,tmp_gray2);
+//  }
+//  *S3 = N * (tmp_gray4) * (1/(pow(tmp_gray2,2))) ;
+//  printf("\n S3=%lf", *S3);
+
+
+  // qua mi devo solo calcolare il quadrato di ogni elemento di matrice e sommarlo --> poi calcolo S$ e S% con le formule
+//  double tmpW2;
+//  for (i=0 ; i<N*N; i++){
+//    tmpW2 = tmpW2 + Wstar[i];
+//    printf("\n i=%d, Wij=%lf, tmpW2=%lf ", i , Wstar[i], tmpW2);
+//  }
+//  *S4= ( pow(N,2) - 3*N + 3 ) * (*S1) - N * (*S2) +  3 * tmpW2;
+//  *S5= ( pow(N,2) - N ) * (*S1) - 2 * N * (*S2) +  6 * tmpW2;
+//  printf("\n S4=%lf, S5=%lf ", *S4, *S5);
+
+ // Mi calcolo finalmente la varianza attesa per Hp nulla
+ //  *varI = ( ( N * (*S4) - (*S3) * (*S5) ) / (( N - 1) * ( N - 2) * ( N - 3) * tmpW2) ) - pow(*expectI,2);
+ //  printf("\n  varI=%lf",*varI);
 }
